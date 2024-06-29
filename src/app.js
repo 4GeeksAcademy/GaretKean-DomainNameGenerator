3cfb4dceb8621ac543c3130322b143e302b22fef
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-06-29 02:20:46 +0000

    Not working

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// let pronoun = ["the", "our"];
+// let adj = ["great", "big"];
+// let noun = ["jogger", "racoon"];
+
+// for (pronoun = 0; pronoun < pronoun.length; pronoun++) {
+// for (adj = 0; adj < adj.length; adj++) {
+// for (noun = 0; noun < noun.length; noun++) {}
+// }
+// console.log(pronoun + adj + noun);
+// }
+// window.onload = function() {
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+for (i = 0; i < pronoun.length; i++) {
+  for (j = 0; j < adj.length; j++) {
+    for (k = 0; k < noun.length; k++) {
+      console.log("${pronoun[i]} + ${adj[j]} + ${noun[k]}.com");
+    }
+  }
+}
+
+// };
