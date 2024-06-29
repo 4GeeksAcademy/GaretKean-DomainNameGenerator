71c10930522d5c062d69db592d9fbaf1ece8caec
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-06-29 04:01:54 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -10,11 +10,15 @@ window.onload = function() {
   let adj = ["great", "big", "sexy"];
   let noun = ["jogger", "racoon", "turd"];
   let loc = [".com", ".us", ".eu", ".edu"];
+  let container = document.getElementById("generated-strings");
   for (let i = 0; i < pro.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < loc.length; l++) {
-          console.log(pro[i] + adj[j] + noun[k] + loc[l]);
+          let generatedString = pro[i] + adj[j] + noun[k] + loc[l];
+          let p = document.createElement("p");
+          p.textContent = generatedString;
+          container.appendChild(p);
         }
       }
     }
