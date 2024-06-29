ba37089f09e16d0427cdd165ce006aadd621333e
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-06-29 03:33:05 +0000

    Works in console

--- src/app.js
+++ src/app.js
@@ -5,27 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-// let pronoun = ["the", "our"];
-// let adj = ["great", "big"];
-// let noun = ["jogger", "racoon"];
-
-// for (pronoun = 0; pronoun < pronoun.length; pronoun++) {
-// for (adj = 0; adj < adj.length; adj++) {
-// for (noun = 0; noun < noun.length; noun++) {}
-// }
-// console.log(pronoun + adj + noun);
-// }
-// window.onload = function() {
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-
-for (i = 0; i < pronoun.length; i++) {
-  for (j = 0; j < adj.length; j++) {
-    for (k = 0; k < noun.length; k++) {
-      console.log("${pronoun[i]} + ${adj[j]} + ${noun[k]}.com");
+window.onload = function() {
+  let pro = ["the", "our", "their"];
+  let adj = ["great", "big", "sexy"];
+  let noun = ["jogger", "racoon", "turd"];
+  let loc = [".com", ".us", ".eu", ".edu"];
+  for (let i = 0; i < pro.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < loc.length; l++) {
+          console.log(pro[i] + adj[j] + noun[k] + loc[l]);
+        }
+      }
     }
   }
-}
-
-// };
+};
